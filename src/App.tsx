d7b49a05f425615ebca543c29f9f434236196579
Rustam <avaraa@mail.ru>
2021-10-13 16:30:14 +0300

    test: small changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload...
-        </p>
+        <p>There will be Chat-test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React.
-        </a>
+        ></a>
       </header>
     </div>
   );
