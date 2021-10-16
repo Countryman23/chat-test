0d241adff238a379eca090de7cf2d5f66658fa67
Rustam <avaraa@mail.ru>
2021-10-16 15:22:39 +0300

    test: install pretty-quick

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>There will be Chat-test</p>
+        <p>There will be8 test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
